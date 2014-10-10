73fc184ffae7894e30fc0aac806d01476991c4cb
Octavi Font <tavifont@gmail.com>
2014-10-10 12:50:07 +0200

    Plot renders and rerenders ok. No cool transformations, but it's usable. So far, only shows elevation.

--- frontend/react_components/Plot.jsx
+++ frontend/react_components/Plot.jsx
@@ -10,35 +10,16 @@ exports.Plot = React.createClass({
     };
   },
   componentDidMount: function() {
-
-  },
-  render: function() {
-    try {
-      this.getDOMNode()
-    } catch (e) {
-      return (
-        <div id="plot" className={this.state.show ? '': 'hide'}>
-        </div>
-      );
-    }
-    if (!this.props.track) {
-      return (
-        <div id="plot" className={this.state.show ? '': 'hide'}>
-        </div>
-      );
-    }
     var holder = this.getDOMNode();
     var margin = {top: 20, right: 20, bottom: 30, left: 50};
     var width = holder.clientWidth - margin.right - margin.left;
     var height = holder.clientHeight - margin.top - margin.bottom;
-
     var x = d3.time.scale()
-      .domain(d3.extent(this.props.track.trackpoints, function(d) { return d.time; }))
       .range([0, width]);
 
     var y = d3.scale.linear()
-      .domain([this.props.track.minElevation(), this.props.track.maxElevation()])
       .range([height, 0]);
+
     var xAxis = d3.svg.axis()
       .scale(x)
       .orient("bottom");
@@ -58,25 +39,46 @@ exports.Plot = React.createClass({
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    svg.append("path")
-      .datum(this.props.track.trackpoints)
-      .attr("class", "area")
-      .attr("d", area);
+    // Make this variables available for render()
+    this.plot = {
+      height: height,
+      svg: svg,
+      x: x,
+      y: y,
+      xAxis: xAxis,
+      yAxis: yAxis,
+      area: area
+    };
+  },
+  render: function() {
 
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    if (this.props.track) {
+      // Remove previous plot
+      this.plot.svg.selectAll("*").remove();
+      // Draw current plot
+      this.plot.x.domain(d3.extent(this.props.track.trackpoints, function(d) { return d.time; }))
+      this.plot.y.domain([this.props.track.minElevation(), this.props.track.maxElevation()]);
 
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("elevation (m)");
+      this.plot.svg.append("path")
+        .datum(this.props.track.trackpoints)
+        .attr("class", "area")
+        .attr("d", this.plot.area);
+
+      this.plot.svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + this.plot.height + ")")
+        .call(this.plot.xAxis);
+
+      this.plot.svg.append("g")
+        .attr("class", "y axis")
+        .call(this.plot.yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("elevation (m)");
+    }
     return (
       <div id="plot" className={this.state.show ? '': 'hide'}>
       </div>
